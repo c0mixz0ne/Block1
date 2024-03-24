192328e6e63b88eb261a2013cf2457a7457a0f13
Anton Aladin <aladin.anton95@gmail.com>
2024-03-24 18:21:51 +0700

    Block1 init

---
+++ Block1/Block1.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+int Min(int k)
+{
+	return k - 1;
+}
+
+int main()
+{
+
+	int k, x, a;
+	printf("Enter k: ");
+	scanf("%d", &k);
+	printf("Enter x: ");
+	scanf("%d", &x);
+	if (k == 1) {
+		printf("x is %d", x);
+		return x;
+	}
+	else {
+		a = Min(k);
+		if (a < x) {
+			printf("a is %d", a);
+			return a;
+		}
+		else {
+			printf("x is %d", x);
+			return x;
+		}
+	}
+
+}
